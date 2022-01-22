3d0e420a5a7ba41a01bfde2671c540fe479529e1
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-01-22 15:49:21 +0530

    Add files via upload

---
+++ module4-solution/JS/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Abhishek", "John", "Jen", "Jason", "Franklin", "Frank", "Trevor", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
